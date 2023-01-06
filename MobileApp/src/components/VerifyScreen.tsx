4761420726af3d4a8384910c54b5c0f169f2f331
Greg Holmes <greg.holmes@tru.id>
2023-01-06 16:45:22 +0000

    Fixed styling issue on Verifyscreen

--- MobileApp/src/components/VerifyScreen.tsx
+++ MobileApp/src/components/VerifyScreen.tsx
@@ -52,7 +52,7 @@ const VerifyScreen = ({
   };
 
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <View style={styles.view}>
       <Text style={styles.heading}>Welcome to the Demo Application</Text>
       <Text style={styles.subHeading}>
         Please enter your verification code to continue.
@@ -75,17 +75,24 @@ const VerifyScreen = ({
 };
 
 const styles = StyleSheet.create({
-  heading: {
-    fontSize: 20,
-    marginBottom: 50,
+  view: {
+    flex: 1,
+    alignItems: 'center',
+    marginTop: 120,
     marginLeft: 20,
     marginRight: 20,
   },
+  heading: {
+    fontSize: 20,
+    marginBottom: 10,
+    marginLeft: 10,
+    marginRight: 10,
+  },
   subHeading: {
     fontSize: 15,
-    marginBottom: 20,
-    marginLeft: 20,
-    marginRight: 20,
+    marginBottom: 10,
+    marginLeft: 10,
+    marginRight: 10,
   },
   input: {
     fontSize: 20,
@@ -96,14 +103,13 @@ const styles = StyleSheet.create({
   button: {
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#1955ff',
     color: '#fff',
     paddingVertical: 10,
     paddingHorizontal: 12,
     borderRadius: 5,
     borderWidth: 2,
     borderColor: '#1955ff',
-    marginTop: 20,
+    marginTop: 10,
     width: '80%',
   },
   buttonText: {
